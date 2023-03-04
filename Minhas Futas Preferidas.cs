58530861182d88302685d4c4b25ac425b652a39f
Breno Luca <brenoluca046@gmail.com>
2023-03-03 22:33:39 -0300

    Primeira Versão

---
+++ Minhas Futas Preferidas.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("...Minhas frutas favoritas...");
+Console.WriteLine("");
+Console.WriteLine("1 - Mexerica");
+Console.WriteLine("");
+Console.WriteLine("2 - Maçã Verde");
+Console.WriteLine("");
+Console.WriteLine("3 - Laranja");
+Console.WriteLine("");
+Console.WriteLine("4 - Banana");
+Console.WriteLine("");
+Console.WriteLine("5 - Manga");
